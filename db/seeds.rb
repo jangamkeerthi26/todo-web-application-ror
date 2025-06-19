79257cd490544fd0795f9e338fd791c5c70d0251
jangamkeerthi26 <keerthi.jangam@everest.engineeing>
2025-06-19 19:23:19 +0530

    Add seed data with historical tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Task.create!(
+  title: "Old Task - February 2024",
+  description: "A task from last year Feb",
+  completed: false,
+  created_at: Date.new(2024, 2, 5)
+)
+
+Task.create!(
+  title: "Old Task - May 2025",
+  description: "A task from last month",
+  completed: true,
+  created_at: 1.month.ago
+)
